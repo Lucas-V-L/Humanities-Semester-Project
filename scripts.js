023e7a10d9a5bf1abf26ca23792924e46654eabb
Your Name <you@example.com>
2023-05-05 00:57:29 -0500

    major progress on scaling

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const frame = document.getElementById("frame");
+const container = document.getElementById("computer_screen");
+const initialHeight = frame.clientHeight;
+
+function scaleScreen() {
+	var height = container.clientHeight;
+	var scale = height / initialHeight;
+	frame.style.transform = "scale("+scale+", "+scale+")";
+}
+
+scaleScreen()
+
+const phoneFrame = document.getElementById("phoneframe");
+const phoneContainer = document.getElementById("phone_screen");
+try {
+	var phoneInitialHeight = phoneFrame.clientHeight;
+}
+catch (error) {console.log("No Phone!")}
+
+function scalePhoneScreen() {
+	try {
+		var height = phoneContainer.clientHeight;
+		var scale = height / phoneInitialHeight;
+		phoneFrame.style.transform = "scale("+scale+", "+scale+")";
+	}
+	catch (error) {console.log("No Phone!")}
+}
+
+scalePhoneScreen();
+window.onresize = (event) => {
+	scalePhoneScreen();
+	scaleScreen();
+};
