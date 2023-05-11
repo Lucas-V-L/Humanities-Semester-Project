d6ad53d79b09e3c31a6f1d82c32f3d68cc2d5b6c
LucasVL <lucas@lucasvl.nl>
2023-05-10 20:38:56 -0500

    screw css zoom(), it's fucking stupid. made my own solution

--- scripts.js
+++ scripts.js
@@ -19,10 +19,23 @@ function toggleCitationDrawer(sourceID) {
 const frame = document.getElementById("frame");
 const container = document.getElementById("computer_screen");
 const initialHeight = frame.clientHeight;
+const initialWidth = frame.clientWidth;
+currentHeight = initialHeight;
+
+function zoomScreen(factor) {
+	factor = 1/factor;
+	currentHeight = initialHeight * factor;
+	frame.style.height = currentHeight + "px";
+	frame.style.width = initialWidth * factor + "px";
+	if (factor === 1) {
+		frame.style.height = initialHeight;
+		frame.style.width = initialWidth;
+	}
+}
 
 function scaleScreen() {
 	var height = container.clientHeight;
-	var scale = height / initialHeight;
+	var scale = height / currentHeight;
 	frame.style.transform = "scale("+scale+", "+scale+")";
 }
 
@@ -31,21 +44,69 @@ scaleScreen()
 const phoneFrame = document.getElementById("phoneframe");
 const phoneContainer = document.getElementById("phone_screen");
 try {
-	var phoneInitialHeight = phoneFrame.clientHeight;
+	phoneInitialHeight = phoneFrame.clientHeight;
+	phoneInitialWidth = phoneFrame.clientWidth;
+	phoneCurrentWidth = phoneInitialWidth;
+	phoneCurrentHeight = phoneInitialHeight;
+
 }
 catch (error) {console.log("No Phone!")}
 
 function scalePhoneScreen() {
 	try {
-		var height = phoneContainer.clientHeight;
-		var scale = height / phoneInitialHeight;
+		let height = phoneContainer.clientHeight;
+		let scale = height / phoneCurrentHeight;
 		phoneFrame.style.transform = "scale("+scale+", "+scale+")";
 	}
 	catch (error) {console.log("No Phone!")}
 }
 
-scalePhoneScreen();
+currentPhoneHeight = phoneInitialHeight;
+function zoomPhoneScreen(factor) {
+        factor = 1/factor;
+        phoneCurrentHeight = phoneInitialHeight * factor;
+        phoneFrame.style.height = phoneCurrentHeight + "px";
+        phoneFrame.style.width = phoneInitialWidth * factor + "px";
+        if (factor === 1) {
+                phoneFrame.style.height = phoneInitialHeight;
+                phoneFrame.style.width = phoneInitialWidth;
+        }
+}
+
 window.onresize = (event) => {
 	scalePhoneScreen();
 	scaleScreen();
 };
+
+currentZoom = 1;
+
+function zoomin() {
+	currentZoom += 0.2;
+	if (currentZoom >= 3) { currentZoom = 3; }
+	zoomScreen(currentZoom);
+	scaleScreen();
+}
+function zoomout() {
+	currentZoom -= 0.2;
+        if (currentZoom <= 0.4) { currentZoom = 0.4; }
+        zoomScreen(currentZoom);
+	scaleScreen();
+}
+
+currentPhoneZoom = 1;
+
+function zoomin_phone() {
+        currentPhoneZoom += 0.2;
+        if (currentPhoneZoom >= 3) { currentPhoneZoom = 3; }
+        zoomPhoneScreen(currentPhoneZoom);
+        scalePhoneScreen();
+}
+function zoomout_phone() {
+        currentPhoneZoom -= 0.2;
+        if (currentPhoneZoom <= 0.4) { currentPhoneZoom = 0.4; }
+        zoomPhoneScreen(currentPhoneZoom);
+        scalePhoneScreen();
+}
+
+scalePhoneScreen();
+
